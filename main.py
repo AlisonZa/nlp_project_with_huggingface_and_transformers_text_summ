8ace8a5c0265e61f56190586db535ab8533570e0
AlisonZa <103375897+AlisonZa@users.noreply.github.com>
2024-11-11 21:29:10 -0300

    Logging implemented

---
+++ main.py
@@ -0,0 +1,9 @@
+from src.textSummarizer.logging import logger
+
+logger.info("Logging is implemented")
+
+
+
+
+# if __name__ == "__main__":
+   
--- src/textSummarizer/logging/__init__.py
+++ src/textSummarizer/logging/__init__.py
@@ -1,22 +1,29 @@
-import logging
 import os
+import sys
+import logging
 from datetime import datetime
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../"))
+logs_path = os.path.join(PROJECT_ROOT, "logs")
 
-logs_path = os.path.join(PROJECT_ROOT, "logs", LOG_FILE )
-
-os.makedirs(logs_path, exist_ok= True)
+# Create the logs directory if it doesn't exist
+os.makedirs(logs_path, exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
+# Set up logging
+file_handler = logging.FileHandler(LOG_FILE_PATH)
+console_handler = logging.StreamHandler(sys.stdout)
+
 logging.basicConfig(
-    filename= LOG_FILE_PATH,
-    format = "[ %(asctime)s ] - %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level = logging.INFO,
+    format="[ %(asctime)s ] - %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+    handlers=[file_handler, console_handler]
 )
 
+logger = logging.getLogger("summarizerlogger")
+
 if __name__ == "__main__":
-    logging.info("Logging has started")
+    logger.info("Logging has started")
